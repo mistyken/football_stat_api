7678607c0c28a9e4a80a31c287006f66637c414b
Kenneth Chen <mistyken@gmail.com>
2019-04-17 17:12:42 -0700

    fixed an issue where params is not present we get 500 error

--- app/controllers/upload_controller.rb
+++ app/controllers/upload_controller.rb
@@ -1,21 +1,29 @@
 class UploadController < ApplicationController
   # POST /players
   def create
-    params[:rushing].each do |rushing|
-      @player = Player.where("player_id = ?", player_params(rushing)[:player_id]).first_or_create!(player_params rushing)
-      @player.Rushing.where("entry_id = ?", rushing_params(rushing)[:entry_id]).first_or_create!(rushing_params rushing)
+    if params[:rushing]
+      params[:rushing].each do |rushing|
+        @player = Player.where("player_id = ?", player_params(rushing)[:player_id]).first_or_create!(player_params rushing)
+        @player.Rushing.where("entry_id = ?", rushing_params(rushing)[:entry_id]).first_or_create!(rushing_params rushing)
+      end
     end
-    params[:receiving].each do |receiving|
-      @player = Player.where("player_id = ?", player_params(receiving)[:player_id]).first_or_create!(player_params receiving)
-      @player.Receiving.where("entry_id = ?", receiving_params(receiving)[:entry_id]).first_or_create!(receiving_params receiving)
+    if params[:receiving]
+      params[:receiving].each do |receiving|
+        @player = Player.where("player_id = ?", player_params(receiving)[:player_id]).first_or_create!(player_params receiving)
+        @player.Receiving.where("entry_id = ?", receiving_params(receiving)[:entry_id]).first_or_create!(receiving_params receiving)
+      end
     end
-    params[:passing].each do |passing|
-      @player = Player.where("player_id = ?", player_params(passing)[:player_id]).first_or_create!(player_params passing)
-      @player.Passing.where("entry_id = ?", passing_params(passing)[:entry_id]).first_or_create!(passing_params passing)
+    if params[:passing]
+      params[:passing].each do |passing|
+        @player = Player.where("player_id = ?", player_params(passing)[:player_id]).first_or_create!(player_params passing)
+        @player.Passing.where("entry_id = ?", passing_params(passing)[:entry_id]).first_or_create!(passing_params passing)
+      end
     end
-    params[:kicking].each do |kicking|
-      @player = Player.where("player_id = ?", player_params(kicking)[:player_id]).first_or_create!(player_params kicking)
-      @player.Kicking.where("entry_id = ?", kicking_params(kicking)[:entry_id]).first_or_create!(kicking_params kicking)
+    if params[:kicking]
+      params[:kicking].each do |kicking|
+        @player = Player.where("player_id = ?", player_params(kicking)[:player_id]).first_or_create!(player_params kicking)
+        @player.Kicking.where("entry_id = ?", kicking_params(kicking)[:entry_id]).first_or_create!(kicking_params kicking)
+      end
     end
   end
 
